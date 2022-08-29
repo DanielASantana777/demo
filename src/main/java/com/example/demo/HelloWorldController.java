deef3336b83378b52c9e7b36953dad2928e54cc9
Daniel <danyasantana2006@gmail.com>
2022-08-29 18:46:23 -0300

    Iniciando um projeto no GitHub

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HelloWorldController {
+
+    //GET, PUT, POST, DELETE
+
+    @GetMapping
+    public List<String> getHelloWorld() {
+        return List.of("Hello", "World");
+    }
+}
